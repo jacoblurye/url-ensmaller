58240a018c18b8763ebd5b541aded31bdcb4b93f
Jacob Lurye <jlurye@college.harvard.edu>
2018-04-19 00:27:36 -0400

    Generated aliases complete.

--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,14 @@ from app import app
 from app.models import URLMap
 from app.forms import URLForm
 from flask import render_template, redirect, url_for, flash, jsonify
+import validators 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = URLForm()
     if form.validate_on_submit():
-        # TODO: validate form.inURL.data
+        if not validators.url(form.url.data):
+            return jsonify(success=False)
         alias = URLMap.insert(form.url.data)
         return jsonify(success=True, alias=alias)
     return render_template('index.html', form=form)
