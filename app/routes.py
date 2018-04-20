29e02819318d3f7b505c15d6b5d75fcb91c7c581
Jacob Lurye <jlurye@college.harvard.edu>
2018-04-20 15:53:26 -0400

    Redirect to index if alias not found.

--- app/routes.py
+++ app/routes.py
@@ -19,4 +19,4 @@ def lookup(alias):
     long_url = URLMap.find_url(alias)
     if long_url:
         return redirect(long_url)
-    return 'Error - URL not found'
+    return redirect(url_for('index'))
